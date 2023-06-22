0ab627b62853c2a30bbbf55c19c8e5e5d85cae2e
Abraham Varguez <abraham@Administrators-MacBook-Pro.local>
2023-06-22 10:03:22 -0400

    close

--- server.js
+++ server.js
@@ -1,33 +1,33 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const path = require('path');
+const path = require("path");
 
 // Set the port number
 const port = 5000;
 
 // Define the static file directories
-app.use(express.static(path.join(__dirname, '/public')));
-app.use('/script', express.static(path.join(__dirname, '/script')));
-app.use('/css', express.static(path.join(__dirname, '/css')));
-app.use('/img', express.static(path.join(__dirname, '/img')));
+app.use(express.static(path.join(__dirname, "/public")));
+app.use("/script", express.static(path.join(__dirname, "/script")));
+app.use("/css", express.static(path.join(__dirname, "/css")));
+app.use("/img", express.static(path.join(__dirname, "/img")));
 // Define the routes
-app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, '/public/index.html'));
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "index.html"));
 });
 
-app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, '/public/about.html'));
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "about.html"));
 });
 
-app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, '/public/api.html'));
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "api.html"));
 });
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, '/public/contact.html'));
-  });
-  app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, '/public/login.html'));
-  });
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "contact.html"));
+});
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "login.html"));
+});
 
 // Start the server
 app.listen(port, () => {
