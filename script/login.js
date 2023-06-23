1f4a2cc52c13631e1a0381a383f7e2b2450c22e4
Abraham Varguez <abraham@AdminisatorsMBP.attlocal.net>
2023-06-23 13:26:31 -0400

    readme

--- script/login.js
+++ script/login.js
@@ -11,100 +11,51 @@ loginLink.addEventListener("click", () => {
   wrapper.classList.remove("active");
 });
 
-//Login/Register Server ----------------------------------------------------------------------------
-const loginForm = document.querySelector(".form-box.login form");
-const registerForm = document.querySelector(".form-box.register form");
-const baseUrl = window.location.origin; // Get the base URL dynamically
+//Login/Register Front ENd ----------------------------------------------------------------------------
 
-// LOGIN ****************************************************
-loginForm.addEventListener("submit", (event) => {
-  event.preventDefault();
+// document.addEventListener("DOMContentLoaded", () => {
+//   const loginForm = document.getElementById("loginForm");
+//   loginForm.addEventListener("submit", handleLoginFormSubmit);
+// });
 
-  // LOGIN VALUES
-  const username = document.querySelector(
-    '.form-box.login input[type="text"]'
-  ).value;
-  const password = document.querySelector(
-    '.form-box.login input[type="password"]'
-  ).value;
+// function handleLoginFormSubmit(event) {
+//   event.preventDefault();
 
-  // OBJECT WITH THE FORM VALUES
-  const formData = {
-    username: username,
-    password: password,
-  };
+//   // Get the entered username and password
+//   const username = document.getElementById("username").value;
+//   const password = document.getElementById("password").value;
 
-  // POST REQUEST TO TH BACKEND
-  fetch(`${baseUrl}/login`, {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify(formData),
-  })
-    .then((response) => {
-      if (response.ok) {
-        // Successful login, perform desired actions (e.g., redirect, show success message)
-        console.log("Login successful");
-        // TODO: Handle success case**************
-      } else {
-        // Login failed, handle the error (e.g., show error message)
-        console.log("Login failed");
-        // TODO: Handle error case*****************
-      }
-    })
-    .catch((error) => {
-      // Error occurred during the request, handle the error (e.g., show error message)
-      console.log("An error occurred during login:", error);
-      // TODO: Handle error case***********************
-    });
-});
+//   // Create an object to hold the login data
+//   const loginData = {
+//     username,
+//     password,
+//   };
 
-//  REGISTER ************************************************************
-registerForm.addEventListener("submit", (event) => {
-  event.preventDefault();
-
-  // REGISTER INPUT VALUES
-  const username = document.querySelector(
-    '.form-box.register input[type="text"]'
-  ).value;
-  const email = document.querySelector(
-    '.form-box.register input[type="email"]'
-  ).value;
-  const password = document.querySelector(
-    '.form-box.register input[type="password"]'
-  ).value;
-
-  const formData = {
-    username: username,
-    email: email,
-    password: password,
-  };
-
-  //POST REQUEST TO THE BACK END
-  fetch(`${baseUrl}/login`, {
-    method: "POST",
-    // An object that specifies the headers to include in the request. Indicate that the request body will be in JSON format.
-    headers: {
-      "Content-Type": "application/json",
-    },
-    // AN object, containing the email and password values, is converted to a JSON
-    body: JSON.stringify(formData),
-  })
-    .then((response) => {
-      if (response.ok) {
-        // Successful registration, perform desired actions (e.g., redirect, show success message)*********
-        console.log("Registration successful");
-        // TODO: Handle success case**********
-      } else {
-        // Registration failed, handle the error (e.g., show error message)
-        console.log("Registration failed");
-        // TODO: Handle error case****************
-      }
-    })
-    .catch((error) => {
-      // Error occurred during the request, handle the error (e.g., show error message)
-      console.log("An error occurred during registration:", error);
-      // TODO: Handle error case*****************
-    });
-});
+//   // Send a POST request to the server
+//   fetch("/login", {
+//     method: "POST",
+//     headers: {
+//       "Content-Type": "application/json",
+//     },
+//     body: JSON.stringify(loginData),
+//   })
+//     .then((response) => response.text())
+//     .then((result) => {
+//       if (result === "Login successful") {
+//         // If login is successful, hide the login form and display the welcome message
+//         document.getElementById("loginForm").style.display = "none";
+//         const welcomeMessage = document.createElement("p");
+//         welcomeMessage.textContent = `Welcome, ${username}!`;
+//         document.getElementById("welcomeMessage").appendChild(welcomeMessage);
+//       } else {
+//         // If login fails, display an error message
+//         const errorMessage = document.createElement("p");
+//         errorMessage.textContent = "Invalid username or password";
+//         document.getElementById("loginForm").appendChild(errorMessage);
+//       }
+//     })
+//     .catch((error) => {
+//       console.error("Error:", error);
+//       // Handle any errors that occurred during the login request
+//     });
+// }
