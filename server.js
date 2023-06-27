bdd931f6224d710a6718d50353298852ed788bf5
Abraham Varguez <abraham@Administrators-MacBook-Pro.local>
2023-06-27 10:07:55 -0400

    final

--- server.js
+++ server.js
@@ -2,9 +2,13 @@ const express = require("express");
 const app = express();
 const path = require("path");
 const mysql = require("mysql2");
+const bodyParser = require("body-parser");
 
 // Set the port number
 const port = 5000;
+
+//middleware
+app.use(bodyParser.urlencoded({ extended: true }));
 
 // Define the static file directories
 app.use(express.static(path.join(__dirname, "/public")));
@@ -12,38 +16,36 @@ app.use("/script", express.static(path.join(__dirname, "/script")));
 app.use("/css", express.static(path.join(__dirname, "/css")));
 app.use("/img", express.static(path.join(__dirname, "/img")));
 // Define the routes
-app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "index.html"));
+app.get("/public/index.html", (req, res) => {
+  res.sendFile(path.join(__dirname, "/public/index.html"));
 });
 
 app.get("/public/about.html", (req, res) => {
-  res.sendFile(path.join(__dirname, "about.html"));
+  res.sendFile(path.join(__dirname, "/public/about.html"));
 });
 
 app.get("/public/api.html", (req, res) => {
-  res.sendFile(path.join(__dirname, "api.html"));
+  res.sendFile(path.join(__dirname, "/public/api.html"));
 });
 app.get("/public/contact.html", (req, res) => {
-  res.sendFile(path.join(__dirname, "contact.html"));
+  res.sendFile(path.join(__dirname, "/public/contact.html"));
 });
 app.get("/public/login.html", (req, res) => {
-  res.sendFile(path.join(__dirname, "login.html"));
+  res.sendFile(path.join(__dirname, "/public/login.html"));
 });
 
 app.get("/public/survey.html", (req, res) => {
-  res.sendFile(path.join(__dirname, "survey.html"));
+  res.sendFile(path.join(__dirname, "/public/survey.html"));
 });
 
-app.use((req, res, next) => {
-  res.status(404).sendFile(path.join(__dirname, "/public/404.html"));
-});
-
-// Start the server
+// Start the servers
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
 });
 
-//MYSQL
+//MYSQL----------------------------------------------------------------------------
+
+//Refrences the database
 const MySQLconnection = mysql.createConnection({
   host: "localhost",
   user: "root",
@@ -51,6 +53,7 @@ const MySQLconnection = mysql.createConnection({
   database: "login-hopehacks",
 });
 
+//Tells us if the database is connected or not
 MySQLconnection.connect((err) => {
   if (err) throw console.log(`Database is not connected`);
   console.log("Databse Connection Succesful!");
@@ -73,17 +76,18 @@ app.post("/register", (req, res) => {
 });
 
 // LOGIN
-app.post("/login", (req, res) => {
+app.post("/login", (req, result) => {
   const { username, password } = req.body;
   const query = `SELECT * FROM users WHERE username = ? AND password = ?`;
-  MySQLconnection.query(query, [username, password], (err, result) => {
+  MySQLconnection.query(query, [username, password], (err, res) => {
     if (err) {
       console.error(err);
       res.status(500).send("Error logging in");
     } else {
       if (result.length > 0) {
         // Login successful
-        req.session.loggedInUser = username; // Store username in session
+        // Store username in session
+        req.session.loggedInUser = username;
         res.status(200).send("Login successful");
       } else {
         // Login failed
