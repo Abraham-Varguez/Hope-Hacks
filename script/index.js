d48b7219747d2ab5bca9e50b22c142e929d53588
Abraham Varguez <abraham@Abrahams-Mac-mini.local>
2024-02-12 09:56:54 -0500

    updated searh bar functionality

--- script/index.js
+++ script/index.js
@@ -5,12 +5,14 @@ let searchQuery = "";
 const APP_ID = "6a3f3274";
 const APP_key = "e98b7066b1e110e26ed03a907ce97899";
 
+// Event listener for form submission
 searchForm.addEventListener("submit", (e) => {
   e.preventDefault();
   searchQuery = e.target.querySelector("input").value;
   fetchAPI();
 });
 
+// Function to perform API search
 async function fetchAPI() {
   const baseURL = `https://api.edamam.com/search?q=${searchQuery}&app_id=${APP_ID}&app_key=${APP_key}&to=20`;
   const response = await fetch(baseURL);
@@ -19,28 +21,37 @@ async function fetchAPI() {
   console.log(data);
 }
 
+// Function to generate HTML for search results
 function generateHTML(results) {
   let generatedHTML = "";
   results.map((result) => {
     generatedHTML += `
-
-    <div class="item">
-
-    <h1 class="title">${result.recipe.label}</h1>
-    <img src="${result.recipe.image}" alt="#">
-
-    <div class="item-data-container">
-    <p class="item-data"> Calories:      ${result.recipe.calories.toFixed(0)}</p>
-    <p class="item-data">Weigth:       ${result.recipe.totalWeight.toFixed(0)} oz</p>
-    <p class="item-data">Cuisine Type:      ${result.recipe.cuisineType} </p>
-    <p class="item-data">${result.recipe.dietLabels}</p>
-    </div>
-    <a class="btn-api" href="${ result.recipe.url }" target="_blank">View Recipe</a>
-
-    </div>
-`;
+      <div class="item">
+        <h1 class="title">${result.recipe.label}</h1>
+        <img src="${result.recipe.image}" alt="#">
+        <div class="item-data-container">
+          <p class="item-data"> Calories: ${result.recipe.calories.toFixed(
+            0
+          )}</p>
+          <p class="item-data">Weigth: ${result.recipe.totalWeight.toFixed(
+            0
+          )} oz</p>
+          <p class="item-data">Cuisine Type: ${result.recipe.cuisineType} </p>
+          <p class="item-data">${result.recipe.dietLabels}</p>
+        </div>
+        <a class="btn-api" href="${
+          result.recipe.url
+        }" target="_blank">View Recipe</a>
+      </div>
+    `;
   });
   searchResultDiv.innerHTML = generatedHTML;
 }
-// dietLabels
-// 10,000 month	API CALLS
+
+// Additional functionality for button click
+const searchButton = document.querySelector(".search__button");
+
+searchButton.addEventListener("click", () => {
+  searchQuery = document.getElementById("searchInput").value;
+  fetchAPI();
+});
